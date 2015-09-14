059bb3f547b81841b747f1afef30208b656eb986
David Moles <david.moles@ucop.edu>
2015-09-14 16:52:04 -0700

    Added unique value counts

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # eoth-tools
 
 Tools for mucking around with [eothxtf](https://github.com/CDLUC3/eothxtf).
+
+## Discoveries
+
+- 5948 files (3305 for 2008, 2643 for 2012)
+- 97 [files with XML problems](https://docs.google.com/spreadsheets/d/19RqAj--Pj2HqHyv5Ipcpz_ydDOHHgvMusEM4N5b1zJo/edit?usp=sharing)
+  (fixed as of revision 45da629)
+
+### DC terms used
+
+Ten Dublin Core terms appear in all 5948 documents. One, `subject`, appears
+only in about 35% of files (about 40% of 2008 files and 29% of 2012).
+
+| term | appears in | min occurrences | max occurrences | median occurrences | unique values |
+| :--- | :--------- | :-------------- | :-------------- | :----------------- | :------------ |
+| coverage | 5948 | 1 | 1 | 1 | 5 |
+| date | 5948 | 2 | 2 | 2 | 2750 |
+| description | 5948 | 1 | 1 | 1 | 1693 |
+| format | 5948 | 1 | 1 | 1 | 1 |
+| identifier | 5948 | 1 | 1 | 1 | 5948 |
+| provenance | 5948 | 1 | 1 | 1 | 5444 |
+| relation | 5948 | 1 | 1 | 1 | 2 |
+| source | 5948 | 1 | 4 | 1 | 3650 |
+| subject | 2099 | 0 | 265 | 0 | 15550 |
+| title | 5948 | 1 | 1 | 1 | 4564 |
+| type | 5948 | 1 | 1 | 1 | 1 |
--- src/main/scala/org/cdlib/eoth/Record.scala
+++ src/main/scala/org/cdlib/eoth/Record.scala
@@ -15,12 +15,12 @@ object Record {
 
   val log = Logger.getLogger(Record.getClass)
 
-  val DC_ALL = List("abstract", "accessRights", "accrualMethod", "accrualPeriodicity", "accrualPolicy", "alternative", "audience", "available", "bibliographicCitation", "conformsTo", "contributor", "coverage", "created", "creator", "date", "dateAccepted", "dateCopyrighted", "dateSubmitted", "description", "educationLevel", "extent", "format", "hasFormat", "hasPart", "hasVersion", "identifier", "instructionalMethod", "isFormatOf", "isPartOf", "isReferencedBy", "isReplacedBy", "isRequiredBy", "isVersionOf", "issued", "language", "license", "mediator", "medium", "modified", "provenance", "publisher", "references", "relation", "replaces", "requires", "rights", "rightsHolder", "source", "spatial", "subject", "tableOfContents", "temporal", "title", "type", "valid")
+  val DC_TERMS = List("abstract", "accessRights", "accrualMethod", "accrualPeriodicity", "accrualPolicy", "alternative", "audience", "available", "bibliographicCitation", "conformsTo", "contributor", "coverage", "created", "creator", "date", "dateAccepted", "dateCopyrighted", "dateSubmitted", "description", "educationLevel", "extent", "format", "hasFormat", "hasPart", "hasVersion", "identifier", "instructionalMethod", "isFormatOf", "isPartOf", "isReferencedBy", "isReplacedBy", "isRequiredBy", "isVersionOf", "issued", "language", "license", "mediator", "medium", "modified", "provenance", "publisher", "references", "relation", "replaces", "requires", "rights", "rightsHolder", "source", "spatial", "subject", "tableOfContents", "temporal", "title", "type", "valid")
 
   def handle(f: File): Map[String, Seq[String]] = {
     print('.')
     val xml = XML.loadFile(f)
-    DC_ALL.map({ dc =>
+    DC_TERMS.map({ dc =>
       dc -> (xml \ dc).map(n => n.text)
     }).toMap
   }
@@ -53,11 +53,11 @@ object Record {
     val exceptions = mutable.Map[String, Exception]()
 
     results.foreach {
-      case (s, m: Map[String, Seq[String]]) =>
-        DC_ALL.foreach({ dc: String =>
-          val count = m(dc).size
-          val counts = allCounts.getOrElse(dc, List[Int]())
-          allCounts(dc) = counts :+ count
+      case (file, termToValue: Map[String, Seq[String]]) =>
+        DC_TERMS.foreach({ term: String =>
+          val count = termToValue(term).size
+          val counts = allCounts.getOrElse(term, List[Int]())
+          allCounts(term) = counts :+ count
         })
       case (s, e: Exception) =>
         exceptions(s) = e
@@ -67,19 +67,57 @@ object Record {
 
     println("------------------------------------------------------------------------------------------------------------------------")
 
-    DC_ALL.foreach({ dc: String =>
+    var dcTermsUsed = List[String]()
+
+    DC_TERMS.foreach({ dc: String =>
       val counts = allCounts.getOrElse(dc, List[Int]())
       val max = counts.max
       if (max > 0) {
+        dcTermsUsed = dcTermsUsed :+ dc
         println(s"$dc: $max")
       }
     })
 
     println("------------------------------------------------------------------------------------------------------------------------")
 
-    exceptions.keys.toList.sorted.foreach({ s =>
-      val e = exceptions(s)
-      println("%s\t%s".format(s, e.getMessage))
+    val valuesForTerms = mutable.Map[String, Set[String]]()
+    results.foreach {
+      case (file, termToValue: Map[String, Seq[String]]) =>
+        dcTermsUsed.foreach({ term =>
+          termToValue.get(term) match {
+            case Some(values) =>
+              val allValues = valuesForTerms.getOrElse(term, Set[String]())
+              valuesForTerms(term) = allValues ++ values
+            case _ =>
+          }
+        })
+      case _ =>
+    }
+
+    println("------------------------------------------------------------------------------------------------------------------------")
+
+    val term_to_max = mutable.Map[String, Int]()
+
+    println("| term | appears in | min occurrences | max occurrences | median occurrences | unique values |")
+    println("| :--- | :--------- | :-------------- | :-------------- | :----------------- | :------------ |")
+    dcTermsUsed.foreach({ term =>
+      val appears_in = allCounts(term).count(p => p > 0)
+      val sorted = allCounts(term).sorted
+      val min_occurrences = sorted.min
+      val max_occurrences = sorted.last
+      val median_occurrences = sorted(sorted.size / 2)
+      val uniqueValues = valuesForTerms(term).size
+      term_to_max(term) = max_occurrences
+      println("| %s | %d | %d | %d | %d | %d |".format(term, appears_in, min_occurrences, max_occurrences, median_occurrences, uniqueValues))
     })
+
+    if (exceptions.nonEmpty) {
+      println("------------------------------------------------------------------------------------------------------------------------")
+      val byMessage: mutable.Map[Exception, String] = exceptions.map { case (s: String, e: Exception) => (e, s) }
+      byMessage.keys.toList.sortBy(e => e.getMessage).foreach({ e =>
+        val s = byMessage(e)
+        println("%s\t%s".format(s, e.getMessage))
+      })
+    }
   }
 }
